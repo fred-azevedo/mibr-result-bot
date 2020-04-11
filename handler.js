54149bd04b7a4e8da0b2ef3cfb51dd2c382d3392
fred-azevedo <fredazevedo89@gmail.com>
2020-04-10 22:04:09 -0300

    Criando primeiro handler e cron para 3 minutos

--- handler.js
+++ handler.js
@@ -1,6 +1,9 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.start = async event => {
+
+  console.log('started');
+
   return {
     statusCode: 200,
     body: JSON.stringify(
